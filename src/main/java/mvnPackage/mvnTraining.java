657a95d9bff2b849ccb3cf91df9d25b9f471d8b0
koenberton <koen.berton@gmail.com>
2018-01-05 10:20:17 +0100

    Initial version

---
+++ src/main/java/mvnPackage/mvnTraining.java
@@ -0,0 +1,10 @@
+package mvnPackage;
+
+public class mvnTraining 
+{
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+		System.out.println ("Hello from mvnTraining within package mvnPackage");
+    }
+}
